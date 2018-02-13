f9ca9dc426c21eda26ca4daaccce2040cca8fd5c
takagotch <dyaccb@gmail.com>
2018-02-13 16:37:40 +0900

    moment().format('MMMM Do YYYY , h:mm:ss a'); npm install --save

---
+++ cmd.sh
@@ -0,0 +1,8 @@
+npm install moment --save
+yarn add moment
+Install-Package Moment.js
+spm install moment --save
+meteor add momentjs:moment
+bower install moment --save
+
+
---
+++ dates.js
@@ -0,0 +1,6 @@
+moment().format('MMMM Do YYYY, h:mm:ss a');
+moment().format('dddd');
+moment().format('MMM Do YY');
+moment().format('YYYY [escaped] YYYY');
+moment().format();
+
